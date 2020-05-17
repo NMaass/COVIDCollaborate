76716134e55e574b88aa14b8dfb88950f519996b
nmaass <nicholasfmaassen@gmail.com>
2020-05-16 20:24:03 -0400

    Updates dependencies and clears out default app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-    <div>this is an empty project</div>
+    <div>this is an empty project</div>,
+document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
