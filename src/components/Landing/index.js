e919e5c14bfea52bba30e10c059fe603f86ad06c
nmaass <nicholasfmaassen@gmail.com>
2020-06-10 17:25:33 -0400

    reverts tutorial and hospital addresses

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,15 +1,8 @@
 import React, {useState} from 'react';
 
 export default function Landing(props) {
-    const [name, setName] = useState('');
-
     return(
         <div>
-            <input
-            type="text"
-            value={name}
-            onChange={event => setName(event.target.value)}
-            />
             <h1> Welcome to COVID collaborate {name}</h1>
         </div>
     )
