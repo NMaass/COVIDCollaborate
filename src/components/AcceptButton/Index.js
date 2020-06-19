3894a4e2151ffdf3c7d759691d16aa6a9131fc52
Mitundas68 <gh9578@wayne.edu>
2020-06-19 15:29:46 -0400

    Fixed Cards Issues

--- src/components/AcceptButton/Index.js
+++ src/components/AcceptButton/Index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./styles.css";
-import Card from "Card";
+import ImgMediaCard from "./Card";
 import { Grid } from "@material-ui/core";
 import {makeStyles} from "@material-ui/core/styles";
 
@@ -20,19 +20,19 @@ export default function AcceptButton() {
     return (
         <Grid container spacing ={4}>
             <Grid item xs={12} sm= {6} md= {4}>
-                <Card/>
+                <ImgMediaCard/>
             </Grid>
             <Grid item xs={12} sm= {6} md= {4}>
-                <Card/>
+                <ImgMediaCard/>
             </Grid>
             <Grid item xs={12} sm= {6} md= {4}>
-                <Card/>
+                <ImgMediaCard/>
             </Grid>
             <Grid item xs={12} sm= {6} md= {4}>
-                <Card/>
+                <ImgMediaCard/>
             </Grid>
             <Grid item xs={12} sm= {6} md= {4}>
-                <Card/>
+                <ImgMediaCard/>
             </Grid>
         </Grid>
     );
