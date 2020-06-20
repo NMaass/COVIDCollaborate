2eca6985ae0da2879e2b4734091f3822dfc3a4a0
Mitundas68 <gh9578@wayne.edu>
2020-06-19 23:27:43 -0400

    Creation of Cards/AcceptButton

--- src/components/AcceptButton/index.js
+++ src/components/AcceptButton/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./styles.css";
 import ImgMediaCard from "./Card";
 import { Grid } from "@material-ui/core";
 import {makeStyles} from "@material-ui/core/styles";
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Donorhome from "../DonorHome";
+
 
 import { withAuthorization } from '../Session';
 //this is where we're going to check user type, more on that later
@@ -6,6 +8,9 @@ const HomePage = () => (
     <div>
         <h1>Home Page</h1>
         <p>Welcome to COVID Collaborate</p>
+        <Donorhome>
+
+        </Donorhome>
     </div>
 );
 
