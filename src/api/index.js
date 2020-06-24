7cd21fbab778fb1ff73f2f77532517d86a8395df
nmaass <nicholasfmaassen@gmail.com>
2020-06-24 18:22:27 -0400

    adds basic user info grabbing and api component

---
+++ src/api/index.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+
+export const getUser = async () => {
+    try {
+        const user = firebase.auth().currentUser;
+        const data = await firebase.firestore()
+            .doc(`users/${user.uid}`)
+            .get();
+        return {
+            ...data.data(),
+            userType: user.userType,
+        };
+    } catch (e) {
+        console.log("get user info failed", e);
+    }
+}
