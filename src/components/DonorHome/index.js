e55189abaf0bfaca774290b001573cfcb4940e3e
Mitundas68 <gh9578@wayne.edu>
2020-06-30 01:09:33 -0400

    Saving Donor Page

---
+++ src/components/DonorHome/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AcceptButton from "../AcceptButton";
+
+export default function Donorhome() {
+
+    return (
+        <AcceptButton>
+
+        </AcceptButton>
+    )
+
+}
