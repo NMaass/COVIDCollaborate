4ca479d475174474b14411655f8e0280827cbf42
Joy Dass <ga8355@gmail.com>
2020-07-07 10:28:09 -0400

    about page style

---
+++ src/components/About/index.js
@@ -0,0 +1,74 @@
+import React from "react";
+import "./styles.css";
+
+export default function AboutPage() {
+    return (
+        <header>
+
+            <div className="ourmission">
+
+                <h1>-Our Mission-</h1>
+            </div>
+
+            <div className="tagline">
+                <h4>"Tag Line Here"</h4>
+            </div>
+
+            &nbsp;
+            <div className="aboutpage">
+                <h1>About Page</h1>
+            </div>
+
+            <div className="tagline">
+                <h4>" In this Page where you will learn more about our app.................."  </h4>
+            </div>
+            &nbsp;
+
+            <div className="leadership">
+                <h1>leadership</h1>
+
+            </div>
+
+            <div className="leadership1">
+                <h1>Nicholas Maassen</h1>
+                <h4> Some text... </h4>
+            </div>
+
+            <div className="leadership2">
+                <h1>Kaniz Khanom</h1>
+                <h4> Some text... </h4>
+            </div>
+
+            <div className="leadership3">
+                <h1>Joy Dass</h1>
+                <h4> Some text... </h4>
+            </div>
+
+            <div className="leadership4">
+                <h1>Birol Guldal</h1>
+                <h4> Some text... </h4>
+            </div>
+
+            <div className="leadership5">
+                <h1>Henry Kisthardt</h1>
+                <h4> Some text... </h4>
+            </div>
+
+            <div className="leadership6">
+                <h1>Mitun Dass</h1>
+                <h4> Some text... </h4>
+            </div>
+
+            &nbsp;
+
+            <div className="Contact">
+                <h3>For More information Contact us Here!</h3>
+                <h4> Contact Info </h4>
+            </div>
+
+
+
+
+        </header>
+    );
+}
