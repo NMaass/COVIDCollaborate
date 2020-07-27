bdbcb006c63a4ecaeb2360ad0aecda2ae840ef37
Henry <ga5374@wayne.edu>
2020-07-26 23:41:32 -0400

    assignment 6 added tests

---
+++ src/components/ItemRequest/test.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {cleanup, fireEvent, render} from '@testing-library/react';
+import App from './App'
+
+
+test('check requests',() => {
+const value = rendor();
+expect(value).toBeTruthy();
+}
