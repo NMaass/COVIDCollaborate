9a171a870a7aa159f3243ef83c96d18c59f79a4c
joydass <ga8355@gmail.com>
2020-07-27 21:45:14 -0400

    Assignment6/AboutPageTes

---
+++ src/components/AboutTest/App.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import './button.css';
+import Button from './button';
+
+function App () {
+
+    return (
+        <div className="App">
+            <header className ="App-header">
+                <Button label="contact us"></Button>
+
+            </header>
+        </div>
+    );
+}
+
+export default App;
